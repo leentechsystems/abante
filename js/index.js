f003eb71c5872975ba5174fae88c34943ffe88ae
leentechsystems <leentechsystems@gmail.com>
2015-07-10 15:11:13 +0800

    conf

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ var app = {
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
         //var networkState = navigator.network.connection.type;
-        window.location = 'http://abante.com.ph/mobile';
+        window.location = 'wwww/slider.html';
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
